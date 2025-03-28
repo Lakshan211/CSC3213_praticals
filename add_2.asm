5c66fbfc6f1a416aad830182d49eb49e0ddd6a2b
Lakshan211 <139627654+Lakshan211@users.noreply.github.com>
2025-03-28 10:09:41 +0530

    Create add_2.asm

---
+++ add_2.asm
@@ -0,0 +1,73 @@
+;Enter the num01 :3
+;Enter the num02 :4
+;Total of num01 and num02 :7
+
+
+.model small
+.stack 100h
+.data
+	msg1 db "Enter the num01 : $"
+	n1 db 0
+	msg2 db "Enter the num02 : $"
+	n2 db 0
+	msg3 db "Total of num01 and num02 : $"
+	n3 db 0
+	
+	.code
+	start:
+		mov ax,@data
+		mov ds,ax
+		
+		mov dx,offset msg1  
+		mov ah,09h
+		int 21h
+		
+		mov ah,01h 
+		int 21h
+		
+		mov dl,al 
+		sub dl,48
+		
+		mov n1,dl
+		
+		mov dl,0ah  ;or 10(next line)
+		mov ah,02h
+		int 21h
+		
+		mov dx,offset msg2 
+		mov ah,09h
+		int 21h
+		
+		mov ah,01h 
+		int 21h
+		
+		mov dl,al
+		sub dl,48
+		
+		mov n2,dl
+		
+		mov dl,0ah  ;or 10(next line)
+		mov ah,02h
+		int 21h
+		
+		mov dx,offset msg3 
+		mov ah,09h
+		int 21h
+		
+		
+		mov dl,n1
+		add dl,n2
+		add dl,48
+		
+		
+
+		mov ah,02h
+		int 21h
+		
+		mov ax,4c00h
+		int 21h
+		
+	end start
+		
+		
+		
